893a30c87facd2552a424796a3b07ae1eb56fc5c
Pablo Fierro Vallejos <pablofierrovallejos@hotmail.com>
2022-08-05 22:52:17 -0400

    Update sketch_dec12a.ino
    Actualiza menú seteo, permite setear segundos en tiempo de encendido y tiempo de apagado.

--- sketch_dec12a.ino
+++ sketch_dec12a.ino
@@ -12,83 +12,80 @@ const int pin_d7 = 7;
 const int pin_BL = 10; 
 LiquidCrystal lcd( pin_RS,  pin_EN,  pin_d4,  pin_d5,  pin_d6,  pin_d7);
 
-
 time_t fecha;                          // Declaramos la variable del tipo time_t
 String tiempoON = "00:00:10";          // Tiempo base para el conteo
-String tiempoOFF = "00:00:10";          // Tiempo base para el conteo
-const int ledPIN = 3;                  // Pin para activar relé
+String tiempoOFF = "00:00:10";         // Tiempo base para el conteo
 String estado;                         // Estado de la salida a activar
-byte dato;                             // El byte que se lee de memoria para base de tiempo en ON
-byte dato2;                            // El byte que se lee de memoria para base de tiempo en OFF
-int modoseteo=0;                       // Modo seteo tiempo 1 = seteando por usuario (presionó SET)
+String tiempoActual="";
+const int ledPIN = 3;                  // Pin para activar relé
+int  modoseteo=0;                      // Modo seteo tiempo 1 = seteando por usuario (presionó SET)
 byte tiemposeteadoRST = 0;
 byte setDuty = 1;                      // Para setear el tiempo de encendido
-byte memon =0;
-byte memoff =0;
-byte cicloSalida = 1;                  //Define el estado actual de la salida
+byte memon =0;                         // Contenido de memoria posicion 0 para el tiempo en on
+byte memoff =0;                        // Contenido de memoria posicion 1 para el tiempo en off
+byte memonS =0;                        // Contenido de memoria posicion 2 para el tiempo en on  segundos
+byte memoffS =0;                       // Contenido de memoria posicion 3 para el tiempo en off segundos
+
+byte cicloSalida = 1;                  // Define el estado actual de la salida
+byte imenu = -1;                        // Opción de menú actual
+
 void setup() {
+  
+  //inicializaMemoria();
+  
   Serial.begin(9600);
-  lcd.begin(16, 2); // Indicar a la libreria que tenemos conectada una pantalla de 16x2 (COLUMNAS x FILAS)
-  lcd.clear();      // Mover el cursor a la primera posición de la pantalla (0, 0)
+  lcd.begin(16, 2);                   // Indicar a la libreria que tenemos conectada una pantalla de 16x2 (COLUMNAS x FILAS)
+  lcd.clear();                        // Mover el cursor a la primera posición de la pantalla (0, 0)
   lcd.print("INICIO TIMER");
   Serial.println("INICIO TIMER");
   
   setTime(0, 0, 0, 12, 12, 2021);     // Establecemos la fecha
   pinMode(ledPIN , OUTPUT);
-  digitalWrite(ledPIN, 1);
+  encenderSalida();
   Serial.println("LED ON");
   estado = "ON";
 
-  memon  = EEPROM.read(0);
-  memoff = EEPROM.read(1);
+  memon   = EEPROM.read(0);
+  memonS  = EEPROM.read(1);
+  memoff  = EEPROM.read(2);
+  memoffS = EEPROM.read(3);
   
   Serial.print("EEPROM ");
-  Serial.println(dato);
-  tiempoON =   formateaHoraCadena(0,int(memon), 0);
-  tiempoOFF =  formateaHoraCadena(0,int(memoff), 0);
-  Serial.print("formateaHoraCadena ");
-  Serial.println(tiempoON);
-  Serial.println(tiempoOFF);
 
+  tiempoON =   formateaHoraCadena(0,int(memon),  int(memonS));
+  tiempoOFF =  formateaHoraCadena(0,int(memoff), int(memoffS));
+  
+}
+void inicializaMemoria(){
+     EEPROM.write(0, 0);
+     EEPROM.write(1, 1);
+     EEPROM.write(2, 0);
+     EEPROM.write(3, 1);
 }
 
-
 void loop() {
+
   Serial.println("Loop -> ");
   Serial.print("Time On  "); Serial.println(tiempoON);
   Serial.print("Time Off "); Serial.println(tiempoOFF);
   if(modoseteo==0){
+      
       fecha = now();
-      String tiempoActual = timeToString(fecha);
-      lcd.clear();
+      tiempoActual = timeToString(fecha);
       lcd.setCursor(0, 0);
       lcd.print("PFIERRO TIMER");
-    
       lcd.setCursor(0, 1);
       lcd.print(tiempoActual + " " + estado);
+
       Serial.println(tiempoActual);
-      
-      delay(1000);
+
       reseteaTiempo(tiempoActual);
 
       leerTeclado();
   }else{
       lcd.setCursor(0, 1);
       leerTeclado();
-      delay(350);
   }
- 
- // midebugmem();
-
-}
-
-void midebugmem(){
-  lcd.print(dato);
-  Serial.print("Read EPROM: >>");
-  Serial.print(dato);
-  Serial.println("<<");
-  Serial.print("Formated: ");
-  Serial.println(formateaHoraCadena(0,int(dato),0));
   delay(1000);
 }
 
@@ -120,29 +117,32 @@ void reseteaTiempo(String tiempoActual) {
   if ( cicloSalida == 1){
       if (tiempoON == tiempoActual) {
         setTime(0, 0, 0, 12, 12, 2021);     // Resetea a cero el reloj de tiempo
-        //intercambiaEstadoLed();
         apagarSalida();
         cicloSalida = 0;
       }
   }else{
       if (tiempoOFF == tiempoActual) {
         setTime(0, 0, 0, 12, 12, 2021);     // Resetea a cero el reloj de tiempo
-        //intercambiaEstadoLed();
         encenderSalida();
         cicloSalida = 1;
       }
   }
+  
 }
 
 void encenderSalida(){
-      estado = "ON";
-      digitalWrite(ledPIN, 1);
-      Serial.println("LED ON");
+      estado = "ON ";
+      digitalWrite(ledPIN, 0);
+      Serial.println("LED ON_");
+      lcd.setCursor(0, 1);
+      lcd.print(tiempoActual + " " + estado);
 }
 void apagarSalida(){
       estado = "OFF";
-      digitalWrite(ledPIN, 0);
+      digitalWrite(ledPIN, 1);
       Serial.println("LED OFF");
+      lcd.setCursor(0, 1);
+      lcd.print(tiempoActual + " " + estado);
 }
 void intercambiaEstadoLed() {
   if (digitalRead(ledPIN)) {
@@ -150,7 +150,7 @@ void intercambiaEstadoLed() {
       digitalWrite(ledPIN, 0);
       Serial.println("LED OFF");
   } else {
-      estado = "ON";
+      estado = "ON ";
       digitalWrite(ledPIN, 1);
       Serial.println("LED ON");
   }
@@ -163,57 +163,40 @@ void leerTeclado() {
   if (x < 60) {               // lcd.print ("Right ");  ALMACENA NUEVO VALOR
          modoseteo= 0;         // Sale del modo SETEO de tiempo
          EEPROM.write(0, memon);
+         EEPROM.write(1, memonS);
          Serial.print("MEM STORE ON: ");
          Serial.println(memon);
          lcd.setCursor(0,0);
          lcd.print("MEM STORE ON: ");
          lcd.setCursor(14,0);
          lcd.print(memon);
-         tiempoON = formateaHoraCadena(0, int(memon),0);
+         tiempoON = formateaHoraCadena(0, int(memon),  int(memonS));
          delay(1500); 
          
          setDuty = 1;
-         EEPROM.write(1, memoff);
+         EEPROM.write(2, memoff);
+         EEPROM.write(3, memoffS);
          Serial.print("MEM STORE OFF: ");
          Serial.println(memoff);
          lcd.setCursor(0,0);
          lcd.print("MEM STORE OFF: ");
          lcd.setCursor(14,0);
          lcd.print(memoff);
-         tiempoOFF = formateaHoraCadena(0, int(memoff),0);
+         tiempoOFF = formateaHoraCadena(0, int(memoff),  int(memoffS));
          delay(1500);
          
   }else if (x < 200){         // lcd.print ("Up    "); 
-        lcd.setCursor(0, 1);
-        lcd.print("   ");
-        lcd.setCursor(0, 1);
-        if( setDuty == 1){
-            memon = memon + 1;
-            lcd.print(memon);
-        }else{
-            memoff = memoff + 1;
-            lcd.print(memoff);
-        }
-        Serial.print("memon ");
-        Serial.print(memon);
-        Serial.print("memoff ");  
-        Serial.println(memoff);
+        lcd.setCursor(0, 1); lcd.print("   "); lcd.setCursor(0, 1);
+        actualizaMemoria(1);
+  
+        Serial.print("memon "); Serial.print(memon); Serial.print(memonS);
+        Serial.print(" memoff "); Serial.print(memoff); Serial.println(memoffS); 
   }else if (x < 400){         // lcd.print ("Down  "); 
-        lcd.setCursor(0, 1);
-        lcd.print("   ");
-        lcd.setCursor(0, 1);
-
-        if( setDuty == 1){
-            memon = memon - 1;
-            lcd.print(memon);
-        }else{
-            memoff = memoff - 1;
-            lcd.print(memoff);
-        }
-        Serial.print("memon ");
-        Serial.print(memon);
-        Serial.print("memoff ");  
-        Serial.println(memoff);
+        lcd.setCursor(0, 1); lcd.print("   "); lcd.setCursor(0, 1);
+        actualizaMemoria(-1);
+        
+        Serial.print("memon "); Serial.print(memon); Serial.print(memonS);
+        Serial.print(" memoff "); Serial.print(memoff); Serial.println(memoffS);  
   }else if (x < 600){   
         lcd.print ("Cancelando...");
         delay(1000); 
@@ -221,30 +204,55 @@ void leerTeclado() {
   }else if (x < 800){         // lcd.print ("Select"); <-----SELECT
         lcd.clear();
         lcd.setCursor(0,0);
-        if( setDuty == 1 ){
-            setDuty = 0;
-        }else{
-            setDuty = 1;
-        }
-        if( setDuty == 1){
-            lcd.print ("SET MIN ON ");
-            lcd.setCursor(0,1);
-            lcd.print(memon);
-
-            Serial.print("EEP_ON ");
-            Serial.println(memon);
-        }else{
-            lcd.print ("SET MIN OFF");
-            lcd.setCursor(0,1);
-            lcd.print(memoff);
-
-            Serial.print("EEP_OF ");
-            Serial.println(memoff);
+        imenu++;
+        if(imenu==4){ imenu=0;}
+        switch(imenu){
+          case 0:                   
+                  lcd.print ("SET MIN ON ");
+                  lcd.setCursor(0,1);
+                  lcd.print(memon);
+                  break;
+          case 1: 
+                  lcd.print ("SET SEC ON ");
+                  lcd.setCursor(0,1);
+                  lcd.print(memonS);
+                  break;
+          case 2: 
+                  lcd.print ("SET MIN OFF");
+                  lcd.setCursor(0,1);
+                  lcd.print(memoff);
+                  break;
+          case 3: 
+                  lcd.print ("SET SEC OFF");
+                  lcd.setCursor(0,1);
+                  lcd.print(memoffS);
+                  break;
         }
         modoseteo= 1;
+
   }
 }
 
+void actualizaMemoria(byte paso){
+    switch(imenu){
+             case 0:
+                    memon = memon + paso;
+                    lcd.print(memon);
+                    break;
+             case 1:
+                    memonS = memonS + paso;
+                    lcd.print(memonS);          
+                    break;
+             case 2:
+                    memoff = memoff + paso;
+                    lcd.print(memoff);          
+                    break;
+             case 3:
+                    memoffS = memoffS + paso;
+                    lcd.print(memoffS);             
+                    break;
+    } 
+}  
 /*Fuente Base 
 #include <LiquidCrystal.h>
 //LCD pin to Arduino
@@ -273,3 +281,5 @@ void loop() {
   else if (x < 400){   lcd.print ("Down  "); }
   else if (x < 600){   lcd.print ("Left  "); }
   else if (x < 800){   lcd.print ("Select"); }
+}
+*/
